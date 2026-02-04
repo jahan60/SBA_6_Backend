c0f0ac366d588e0b9f6730ffa2ab0b34fa9ba9b7
jahan60 <faridajahan.ny@gmail.com>
2026-02-04 18:56:05 -0500

    get all and get by id routes are working fine

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -4,14 +4,34 @@ import Product from "../Models/ProductSchema.js";
 
 const router = express.Router();
 
-//Create 
+//Create Route
 router
 .route("/")
 .post(async (req, res)=> {
-    let newProduct = await Product.inserOne(req.body);
+    let newProduct = await Product.insertOne(req.body);
     res.json(newProduct);
-})
+});
 
+//Get Route
+// get by id 
+router
+.route("/:id")
+.get(async (req, res)=>{
+    let product = await Product.findById(req.params.id);
+
+    if(!product) return res.status(404).json({ error: "Product Not Found"});
+    res.json(product);
+});
+//get all 
+router
+.route("/")
+.get(async (req, res)=>{
+    let product = await Product.find();
+
+    if(!product) return res.status(404).json({ error: "Product Not Found"});
+    res.json(product);
+});
+//update 
 
 
 
