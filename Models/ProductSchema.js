a20f292b6ae751a2ba7b9420a98cb145ca530746
jahan60 <faridajahan.ny@gmail.com>
2026-02-04 18:06:12 -0500

    Get route working for the Product collection

---
+++ Models/ProductSchema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+
+        price : {
+            type: Number,
+            required: true
+        },
+        category: {
+            type: String,
+            required: true
+        },
+
+        inStocj: {
+            type: Boolean,
+            default: true
+        }
+    },
+    { timestamps: true}
+
+);
+
+
+export default mongoose.model("Product", productSchema);
--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-
-
+import Product from "../Models/ProductSchema.js";
 
 
 const router = express.Router();
+
+//Create 
+router
+.route("/")
+.post(async (req, res)=> {
+    let newProduct = await Product.inserOne(req.body);
+    res.json(newProduct);
+})
+
+
+
 
 
 export default router;
