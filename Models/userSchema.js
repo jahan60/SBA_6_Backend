acdb0c072adab1dcff493f15bba3261e76e074bb
jahan60 <faridajahan.ny@gmail.com>
2026-02-06 19:53:46 -0500

    Virtuals added for User routes

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -26,7 +26,28 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+//Virtuals
+userSchema.set("toJSON", { virtuals: true });
 
+//virtual :id 
+userSchema.virtual("id").get(function () {
+  return this._id.toString();
+});
+
+//Virtual for exact first name 
+userSchema.virtual("firstName").get(function () {
+  return this.name.split(" ")[0];
+});
+
+//virtuals for last 4 digit phone number 
+userSchema.virtual("phoneLast4").get(function () {
+  return this.phoneNumber.slice(-4);
+});
+
+//Password length 
+userSchema.virtual("maskedPassword").get(function () {
+  return "*".repeat(this.password.length);
+});
 
 
 //Export Model
