76e22a9b6aa08773c24fe43ea9e5c966797ac21d
jahan60 <faridajahan.ny@gmail.com>
2026-02-07 20:53:35 -0500

    Virtuals added in Order Schema

--- Models/orderSchema.js
+++ Models/orderSchema.js
@@ -28,5 +28,41 @@ const orderSchema = new mongoose.Schema(
 
 
 
+// Virtuals
+
+orderSchema.set("toJSON", { virtuals: true });
+
+// Virtual: id
+orderSchema.virtual("id").get(function () {
+  return this._id.toString();
+});
+
+// Virtual: formattedAmount
+orderSchema.virtual("formattedAmount").get(function () {
+  return `$${this.amount.toFixed(2)}`;
+});
+
+// Virtual: isLargeOrder
+orderSchema.virtual("isLargeOrder").get(function () {
+  return this.amount > 100;
+});
+
+// Virtual: summary
+orderSchema.virtual("summary").get(function () {
+  return `Order ${this._id.toString()} by User ${this.userId}`;
+});
+
+// Virtual: statusMessage 
+orderSchema.virtual("statusMessage").get(function () {
+  const messages = {
+    pending: "Your order is being processed",
+    shipped: "Your order is on the way",
+    completed: "Your order has been delivered",
+    cancelled: "Your order was cancelled"
+  };
+
+  return messages[this.status] || "Unknown status";
+});
+
 // Export Model
 export default mongoose.model("order", orderSchema);
